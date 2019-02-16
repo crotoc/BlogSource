e947e4a7fb830e7426068431d2a8ce8947e675b5
crotoc <crotoc@gmail.com>
2019-02-15 19:49:02 -0600

    add a url of qq for shieldTV

---
+++ ss2dnsmasq.sh
@@ -0,0 +1,1 @@
+perl -lane 'if(!/\!/){print "ipset=/$_/ss_rules_dst_unblock"}' source/_posts/Unblock-region-restriction-of-musics-videos/unblock.rules.txt > source/_posts/Unblock-region-restriction-of-musics-videos/unblock.rules.dnsmasq.conf 
