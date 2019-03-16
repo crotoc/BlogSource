aa932827e8b9660d61a3808781d966c2a9cf3cec
crotoc <crotoc@gmail.com>
2019-03-16 01:37:22 -0500

    Commit again for travis deploy

--- ss2dnsmasq.sh
+++ ss2dnsmasq.sh
@@ -1,2 +1,3 @@
 perl -lane 'if(!/\!/){print "ipset=/$_/ss_rules_dst_unblock"}' source/_posts/Unblock-region-restriction-of-musics-videos/unblock.rules.txt > source/_posts/Unblock-region-restriction-of-musics-videos/unblock.rules.dnsmasq.conf 
+
 perl -lane 'if(!/\!/){print "ipset=/$_/unblock-youku"}' source/_posts/Unblock-region-restriction-of-musics-videos/unblock.rules.txt > source/_posts/Unblock-region-restriction-of-musics-videos/unblock.rules.v2ray.dnsmasq.conf 
